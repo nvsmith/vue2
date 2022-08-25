711157c503e5b02f96337ed695089f057fcd569a
Nathan Smith <nathanielvs@gmail.com>
2022-08-24 23:41:53 -0600

    12 - v-for loops

---
+++ 12-vfor-loops/app.js
@@ -0,0 +1,16 @@
+new Vue({
+  el: "#vue-app",
+
+  data: {
+    characters: ["Meg", "Jo", "Beth", "Amy"],
+    athletes: [
+      { name: "Tyson", age: 22 },
+      { name: "Bolt", age: 28 },
+      { name: "Phelps", age: 25 },
+    ],
+  }, // end data
+
+  methods: {}, //end methods
+
+  computed: {}, // end computed
+});
