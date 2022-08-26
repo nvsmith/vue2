a5bcfbdfa39dbbb0ea04699773acf0d2b7b6868e
Nathan Smith <nathanielvs@gmail.com>
2022-08-25 22:29:51 -0600

    13 - punching bag

---
+++ 13-punching-bag/app.js
@@ -0,0 +1,24 @@
+new Vue({
+  el: "#vue-app",
+
+  data: {
+    health: 100,
+    ended: false,
+  }, // end data
+
+  methods: {
+    punch: function () {
+      this.health -= 10;
+      if (this.health <= 0) {
+        this.ended = true;
+      }
+    },
+
+    restart: function () {
+      this.health = 100;
+      this.ended = false;
+    },
+  }, //end methods
+
+  computed: {}, // end computed
+});
